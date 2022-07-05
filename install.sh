b05f6b10ce97b9a25b0610fa2a86ff09f427cd5e
Minh Sang <88425701+nonesang@users.noreply.github.com>
2022-07-05 17:26:14 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S xorg-server xorg-apps xorg-xinit xf86-video-intel i3-gaps i3status i3lock lightdm lightdm-gtk-greeter bluez bluez-utils ranger rofi pulseaudio pulseaudio-bluetooth alsa-utils alsa-plugins fcitx fcitx-unikey fcitx-im fcitx-configtool xclip scrot feh neovim xterm mpv xdg-user-dirs ttf-liberation ttf-droid ttf-roboto terminus-font ttf-dejavu ttf-freefont ttf-fira-code 
+sudo pacman -S xorg-server xorg-apps xorg-xinit xf86-video-intel i3-gaps i3status i3lock lightdm lightdm-gtk-greeter bluez bluez-utils ranger rofi pulseaudio pulseaudio-bluetooth alsa-utils alsa-plugins fcitx fcitx-unikey fcitx-im fcitx-configtool xclip scrot feh neovim xterm mpv xdg-user-dirs ttf-liberation ttf-droid ttf-roboto terminus-font ttf-dejavu ttf-freefont ttf-fira-code picom 
 
 sudo systemctl enable bluetooth
 sudo systemctl enable lightdm.service
